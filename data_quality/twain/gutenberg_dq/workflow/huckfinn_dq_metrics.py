661230faa453300818a49da0f7db2734a22e80d9
jarmoza <jarmoza@gmail.com>
2024-01-01 16:14:51 -0500

    Instrinsic cumulative match update

--- data_quality/twain/gutenberg_dq/workflow/huckfinn_dq_metrics.py
+++ data_quality/twain/gutenberg_dq/workflow/huckfinn_dq_metrics.py
@@ -15,6 +15,8 @@ considered to be the primary/reputable edition
 
 Intrinsic dimensions include accuracy, objectivity, believability, and reputation
 
+Intrinsic accuracy - 
+
 ===============================================================================
 Contextual data quality
 
@@ -66,58 +68,62 @@ from utilities import aolm_paths
 
 # Classes 
 
-# This object knows how to ingest HuckFinn metadata
-class HuckFinnDQMetric(DataQualityMetric):
+
+        
+
+class HuckFinnDQ_IntrinsicCumulativeMatch(HuckFinnDQMetric):
 
     # Constructor
 
-    def __init__(self, p_name, p_metadata_json):
+    def __init__(self, p_source_metadata_json, p_compared_metadata_json):
 
-        # 1. Call the base data quality class constructor
-        super().__init__(p_name, p_metadata_json)
+        # 1. Call the base Huck Finn data quality class constructor
+        super().__init__("IntrinsicCumulativeMatch", p_compared_metadata_json)
 
-    # HuckFunn-specific properties from its metadata
-    @property
-    def clean_components(self):
-        return self.m_input["clean_components"]
-    @property
-    def components(self):
-        return self.m_input["components"]
-    @property
-    def cumulative_word_counts(self):
-        return self.m_input["cumulative_word_counts"]
-    @property
-    def flat_components(self):
-        return self.m_input["flat_components"]
-    @property
-    def top_words(self):
-        return self.m_input["top_words"]
-    @property
-    def top_word_counts_by_chapter(self):
-        return self.m_input["top_word_counts_by_chapter"]
-    @property
-    def total_word_frequencies(self):
-        return self.m_input["total_word_frequencies"]
-    @property
-    def word_counts(self):
-        return self.m_input["word_counts"]
+        # 2. Save the source (ground truth) metadata json
+        self.m_source_metadata_json = p_source_metadata_json
 
-    # Specialized properties
+    # Properties
 
     @property
-    def metadata(self):
-        return self.m_input
+    def source_metadata(self):
+        return self.m_source_metadata_json
 
-
-    # Required interface methods
-    @abstractmethod
+    # Required methods
     def compute(self):
-        pass
-
-    def output(self):
-        return { "metric": self.m_result }
         
+        # Data quality metric: Comparing cumulative word counts by chapter
+        # Goal: Calculate +/- word counts (and, implicitly, missing words)
+        #       and do so by chapter instead of via overall count
 
+        # NOTE: There is an expected guarantee that each text has been curated enough
+        # to have a) the same number of chapters and b) the same key for each chapter name
+        
+        # 0. HuckFinn metadata key for comparison
+        key = "cumulative_word_counts"
+
+        # 1. Calculate the total words in each chapter in each text
+        total_words_per_chapter = [ sum(self.metadata[key][chapter].values()) \
+            for chapter in self.metadata[key] ]
+        source_total_words_per_chapter = [ sum(self.souce_metadata[key][chapter].values()) \
+            for chapter in self.souce_metadata[key] ]
+
+        # 2. Create an overall +/- tally of word counts for each chapter
+        word_match_tallies = { chapter: 0 for chapter in self.metadata[key] }
+        for chapter in self.source_metadata[key]:
+            for word in self.source_metadata[key][chapter]:
+            
+                # A. Check to see if word in source text chapter is in the corresponding text chapter
+                if word in self.metadata[key][chapter]:
+                    word_match_tallies[chapter] += self.metadata[key][chapter][word] - self.source_metadata[key][chapter][word]
+                # B. If the word is not in this text's chapter, subtract the source text chapter's frequency from the tally
+                else:
+                    word_match_tallies[chapter] -= self.source_metadata[key][chapter][word]
+
+        # 3. Determine the overall intrinsic, percent match metric
+        
+        # Let's try the word match tally first
+        self.m_result = word_match_tallies
 
 class HuckFinnDQ_IntrinsicOverallMatch(HuckFinnDQMetric):
 
